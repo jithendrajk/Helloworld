7b41a66011a5b2a3b66263bc656471ac3aea846d
Jithendra <Jithendra.pegaa@gmail.com>
2020-05-13 14:33:57 +0530

    first commit is done

---
+++ src/test/java/packageHelloworld/Demohelloworld.java
@@ -0,0 +1,15 @@
+package packageHelloworld;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demohelloworld {
+	
+ @Test
+ 
+public void testhelloworld() {
+	//System.out.println("..Hellow world");
+	Reporter.log("@Test",true);
+}
+	
+
+}
